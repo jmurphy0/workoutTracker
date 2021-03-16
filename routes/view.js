b2b2bca99d071ecae98bb6ff0922c53883c1a294
John Murphy <murphbot55@gmail.com>
2021-03-16 18:36:33 -0400

    commenting view.js

--- routes/view.js
+++ routes/view.js
@@ -4,11 +4,11 @@
 const router = require("express").Router();
 //const { get } = require("http");
 const path = require("path");
-
+// create the read path to index.html
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
-
+// create the read path to stats.html
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
