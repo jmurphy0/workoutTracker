e282c0b5695fffff90fd35878f68246be4dd5063
John Murphy <murphbot55@gmail.com>
2021-03-16 18:35:34 -0400

    commenting api.js

--- routes/api.js
+++ routes/api.js
@@ -7,7 +7,7 @@
 const router = require("express").Router();
 
 var db = require("../models");
-
+// gets the workoutdata/aggregate from db for index.html
 router.get("/api/workouts", (req, res) => {
   db.Workout.aggregate([
     { $addFields: { totalDuration: { $sum: `$exercises.duration` } } },
@@ -19,7 +19,7 @@ router.get("/api/workouts", (req, res) => {
       .catch((err) => res.json(err))
   );
 });
-
+//gets the workoutdata/aggregate from db for stats.html
 router.get("/api/workouts/range", (req, res) => {
   db.Workout.aggregate([
     { $addFields: { totalDuration: { $sum: `$exercises.duration` } } },
@@ -31,13 +31,13 @@ router.get("/api/workouts/range", (req, res) => {
     })
     .catch((err) => res.json(err));
 });
-
+// create a new workout from user input then store in db
 router.post("/api/workouts", async (req, res) => {
   const cb = await db.Workout.create(req.body).then((workouts) => {
     res.json(workouts);
   });
 });
-
+// updates an exsisting workout based on ID
 router.put("/api/workouts/:id", (req, res) => {
   console.log("yoyoyoyo", req.body);
   db.Workout.findByIdAndUpdate(
